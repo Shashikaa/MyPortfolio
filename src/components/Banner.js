073cc16a57c0e869bacff4cec38922e8322c035e
Shashikaa <shashikaa1452@gmail.com>
2024-07-24 21:32:03 +0530

    Initial commit

---
+++ src/components/Banner.js
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assests/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer", "Android Developer" ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hi! I'm Shashika `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer","Android Developer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>I am a dedicated and passionate AI/Software Developer with a strong background in front-end and back-end development. My expertise includes working with React, Node.js, Redux, and various other technologies. I am currently seeking a full-time remote internship where I can leverage my skills to contribute to innovative projects. I am pursuing my studies at SLIIT City University, which has provided me with a solid foundation in computer science and software engineering.</p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
---
+++ src/components/Skills.js
@@ -0,0 +1,67 @@
+import { Col, Container, Row } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import meter1 from "../assests/img/meter1.svg";
+import meter2 from "../assests/img/meter2.svg";
+import meter3 from "../assests/img/meter3.svg";
+import colorSharp from "../assests/img/color-sharp.png";
+
+
+export const Skills = () => {
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+
+return (
+
+    <section className="skill" id="skills">
+        <Container>
+            <Row>
+                <Col>
+                <div className="skill-bx">
+                    <h2>Skills</h2>
+                    <p>I am proficient in a wide range of programming languages and frameworks, with a strong focus on AI development and software development. My skill set includes expertise in JavaScript, TypeScript, React, Node.js, and Redux for front-end and back-end development.</p>
+                    <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                        <div className="item">
+                            <img src={meter1} alt="Image" />
+                            <h5>Web Development</h5>
+                        </div>
+                        <div className="item">
+                            <img src={meter3} alt="Image" />
+                            <h5>Andriod App Development</h5>
+                        </div>
+                        <div className="item">
+                            <img src={meter2} alt="Image" />
+                            <h5>UI/UX Design</h5>
+                        </div>
+                        <div className="item">
+                            <img src={meter1} alt="Image" />
+                            <h5>Team Work</h5>
+                        </div>
+
+                    </Carousel>
+                </div>
+                </Col>
+            </Row>
+        </Container>
+    <img src={colorSharp} className="background-image-left" />
+    </section>
+)
+
+}
