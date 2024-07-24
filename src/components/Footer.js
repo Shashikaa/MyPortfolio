4f1f0ad003b6a8bcc23095b59ff3a00b38d67d80
Shashikaa <shashikaa1452@gmail.com>
2024-07-24 22:07:49 +0530

    Initial commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailChimpForm";
-import logo from "../assests/img/logo.svg";
 import navIcon1 from "../assests/img/nav-icon1.svg";
 import navIcon2 from "../assests/img/nav-icon2.svg";
 import navIcon3 from "../assests/img/nav-icon3.svg";
@@ -18,7 +17,7 @@ export const Footer = () => {
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/shashika-anushan-b524b21b3/"><img src={navIcon1} alt="Icon" /></a>
               <a href="https://www.facebook.com/shashika.anushan/"><img src={navIcon2} alt="Icon" /></a>
-              <a href=""><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.facebook.com/shashika.anushan/"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Copyright 2023. All Rights Reserved</p>
           </Col>
